e9011bc75005cf2738cba5357db22e67c08fcc46
Anton Kalchenko <anton11042002@gmail.com>
2023-10-01 16:36:15 +0300

    clear

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import json
 import argparse
+import json
 import pathlib
+
 import requests
 from bs4 import BeautifulSoup
-
 from connect import connect
 from models import Authors, Quotes
 
@@ -14,13 +14,13 @@ parser.add_argument("--action", "--a")
 
 
 def next_page():
-    links = ['/']
+    links = ["/"]
     for l in links:
-        html_doc = requests.get(base_url+l)
+        html_doc = requests.get(base_url + l)
         soup = BeautifulSoup(html_doc.text, "html.parser")
         content = soup.select_one("div[class='col-md-8'] nav")
         try:
-            next_link = content.find('li', attrs={'class':'next'}).find('a')['href']
+            next_link = content.find("li", attrs={"class": "next"}).find("a")["href"]
             links.append(next_link)
         except AttributeError:
             continue
@@ -29,49 +29,61 @@ def next_page():
 
 def create_jsons():
     result_quote = []
-    result_autor = []
+    result_author = []
 
     for link in next_page():
-        html_doc = requests.get(base_url+link)
+        html_doc = requests.get(base_url + link)
         soup = BeautifulSoup(html_doc.text, "html.parser")
         content_quote = soup.select("div[class='quote']")
 
         for el in content_quote:
-
-            quote = el.find("span", attrs={'class':"text"}).text
-            author = el.find('small', attrs={"class":"author"}).text
+            quote = el.find("span", attrs={"class": "text"}).text
+            author = el.find("small", attrs={"class": "author"}).text
             tags = [tag.text for tag in el.select('div[class="tags"] a')]
 
-            result_quote.append({"quote":quote, "author":author, "tags":tags})
+            result_quote.append({"quote": quote, "author": author, "tags": tags})
 
-            link_to_bio = el.select_one("span a").get('href')
+            link_to_bio = el.select_one("span a").get("href")
 
-            html_doc_bio = requests.get(base_url+link_to_bio)
+            html_doc_bio = requests.get(base_url + link_to_bio)
             soup_bio = BeautifulSoup(html_doc_bio.text, "html.parser")
             content_bio = soup_bio.select_one("div[class='author-details']")
 
-            fullname= content_bio.find('h3').text
-            born_date= content_bio.find("p").find("span", attrs={"class":"author-born-date"}).text
-            born_location= content_bio.find("p").find("span", attrs={"class":"author-born-location"}).text
-            description= content_bio.find("div", attrs={"class":"author-description"}).text.strip()
+            fullname = content_bio.find("h3").text
+            born_date = (
+                content_bio.find("p")
+                .find("span", attrs={"class": "author-born-date"})
+                .text
+            )
+            born_location = (
+                content_bio.find("p")
+                .find("span", attrs={"class": "author-born-location"})
+                .text
+            )
+            description = content_bio.find(
+                "div", attrs={"class": "author-description"}
+            ).text.strip()
 
-            result_autor.append({"fullname":fullname, "born_date":born_date, "born_location":born_location, "description":description})
-
+            result_author.append(
+                {
+                    "fullname": fullname,
+                    "born_date": born_date,
+                    "born_location": born_location,
+                    "description": description,
+                }
+            )
 
     pathlib.Path("json").mkdir(parents=True, exist_ok=True)
 
-    with open('json/quotes.json', 'w', encoding='utf-8') as fd:
-        json.dump(result_quote, fd, ensure_ascii=False,  indent=2)
+    with open("json/quotes.json", "w", encoding="utf-8") as fd:
+        json.dump(result_quote, fd, ensure_ascii=False, indent=2)
 
-    with open('json/authors.json', 'w', encoding='utf-8') as fd:
-        json.dump(result_autor, fd, ensure_ascii=False,  indent=2)
-
-
-        
+    with open("json/authors.json", "w", encoding="utf-8") as fd:
+        json.dump(result_author, fd, ensure_ascii=False, indent=2)
 
 
 def load_json():
-    with open("json/authors.json", 'r', encoding='utf-8') as json_file:
+    with open("json/authors.json", "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
 
         for i in data:
@@ -83,7 +95,7 @@ def load_json():
             )
             author.save()
 
-    with open("json/quotes.json", 'r', encoding='utf-8') as json_file:
+    with open("json/quotes.json", "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
 
         for i in data:
